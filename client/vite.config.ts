a502b0c17f5633310947f3c20cb5557011066970
Harsh Pandey <hpandey793@gmail.com>
2022-07-29 20:38:32 -0400

    get APP_SECRET working

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,14 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 
-export default defineConfig({
-  build: { target: "esnext" },
-  define: {
-    "process.env": { APP_SECRET: process.env.APP_SECRET },
-  },
-  server: { host: "0.0.0.0" },
-  clearScreen: false,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, "../", "");
+  return {
+    build: { target: "esnext" },
+    define: {
+      "process.env": { APP_SECRET: env.APP_SECRET },
+    },
+    server: { host: "0.0.0.0" },
+    clearScreen: false,
+    envDir: "../",
+  };
 });
--- shared/base.ts
+++ shared/base.ts
@@ -1,4 +1,15 @@
+import path from "path";
+import url from "url";
+import dotenv from "dotenv";
 import { sha256 } from "crypto-hash";
 
-export const APP_SECRET = process.env.APP_SECRET!;
+if (process.env.APP_SECRET === undefined) {
+  const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
+  dotenv.config({ path: path.resolve(__dirname, "../.env") });
+  if (process.env.APP_SECRET === undefined) {
+    throw new Error("APP_SECRET env variable must be set");
+  }
+}
+
+export const APP_SECRET = process.env.APP_SECRET;
 export const APP_ID = await sha256(APP_SECRET);
