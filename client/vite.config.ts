867dbc3886be787943e42e2dc0b3bb04d3a06167
Harsh Pandey <hpandey793@gmail.com>
2022-07-31 14:49:44 -0400

    set appId programatically

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig, loadEnv } from "vite";
+import hash from "hash.js";
 
 export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, "../", "");
+  const appSecret = process.env.APP_SECRET ?? env.APP_SECRET;
+  process.env.APP_ID = hash.sha256().update(appSecret).digest("hex");
   return {
     build: { target: "esnext" },
     server: { host: "0.0.0.0" },
