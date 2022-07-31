3fb13cef4d0c63ea12c527219de0b5df17fd3d82
Harsh Pandey <hpandey793@gmail.com>
2022-07-31 11:51:23 -0400

    improve env handling

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,15 +1,12 @@
 import { defineConfig, loadEnv } from "vite";
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, "../", "");
   return {
     build: { target: "esnext" },
-    define: {
-      "process.env": { APP_SECRET: env.APP_SECRET },
-    },
     server: { host: "0.0.0.0" },
     clearScreen: false,
     envDir: "../",
+    envPrefix: "APP_ID",
     publicDir: "src/assets",
   };
 });
