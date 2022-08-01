f2b8c0d2cc5f78059a9638ba37d7504b85e9c61c
Justin Young <justin_dean_young@yahoo.com>
2022-08-01 01:44:22 -0400

    chest commit

--- server/server.ts
+++ server/server.ts
@@ -2,11 +2,12 @@ import { register } from "@hathora/server-sdk";
 import dotenv from "dotenv";
 
 import { ClientMessage, ClientMessageType, Direction, ServerMessage, ServerMessageType } from "../shared/messages.js";
-import { GameState } from "../shared/state.js";
+import { Chest, Difficulty, GameState } from "../shared/state.js";
 import mapData from "../shared/HAT_mainmap.json" assert { type: "json" };
 
 type RoomId = bigint;
 type UserId = string;
+const numChests = 15;
 
 const PLAYER_SPEED = 10;
 type ServerPlayer = {
@@ -15,8 +16,10 @@ type ServerPlayer = {
   y: number;
   direction: Direction;
 };
+
 type ServerState = {
   players: ServerPlayer[];
+  chests: Chest[];
 };
 const states: Map<RoomId, { subscribers: Set<UserId>; game: ServerState }> = new Map();
 
@@ -30,14 +33,39 @@ const coordinator = await register({
   authInfo: { anonymous: { separator: "-" } },
   store: {
     newState(roomId, userId, data) {
+      //load up chests here
+      let tempChestArray: Chest[] = [];
+      for (let index = 0; index < numChests; index++) {
+        //find random beach spot
+        let newSpot;
+        do {
+          newSpot = {
+            x: Math.floor(Math.random() * 128),
+            y: Math.floor(Math.random() * 64),
+          };
+        } while (!isBeachTile(newSpot));
+        let newReward = 1 + Math.floor(Math.random() * 3);
+        let tempDiff = Math.floor(Math.random() * 3);
+        let newDifficulty: Difficulty = Math.floor(Math.random() * 3);
+        let newID = Math.random().toString(36).substring(2);
+        tempChestArray.push({
+          id: newID,
+          x: newSpot.x,
+          y: newSpot.y,
+          reward: newReward,
+          difficulty: newDifficulty,
+        });
+        //load up chest
+      }
+      console.log(tempChestArray);
       console.log("newState", roomId.toString(36), userId, data);
-      states.set(roomId, { subscribers: new Set(), game: { players: [] } });
+      states.set(roomId, { subscribers: new Set(), game: { players: [], chests: tempChestArray } });
     },
     subscribeUser(roomId, userId) {
       console.log("subscribeUser", roomId.toString(36), userId);
       const { subscribers, game } = states.get(roomId)!;
       subscribers.add(userId);
-      if (!game.players.some((player) => player.id === userId)) {
+      if (!game.players.some(player => player.id === userId)) {
         game.players.push({ id: userId, x: 650, y: 550, direction: Direction.None });
       }
     },
@@ -54,7 +82,7 @@ const coordinator = await register({
       const { game } = states.get(roomId)!;
       const message: ClientMessage = JSON.parse(dataStr);
       if (message.type === ClientMessageType.SetDirection) {
-        const player = game.players.find((p) => p.id === userId);
+        const player = game.players.find(p => p.id === userId);
         if (player !== undefined) {
           player.direction = message.direction;
         }
@@ -65,9 +93,10 @@ const coordinator = await register({
 
 function broadcastUpdates(roomId: RoomId) {
   const { subscribers, game } = states.get(roomId)!;
-  subscribers.forEach((userId) => {
+  subscribers.forEach(userId => {
     const gameState: GameState = {
-      players: game.players.map((player) => ({ id: player.id, x: player.x, y: player.y })),
+      players: game.players.map(player => ({ id: player.id, x: player.x, y: player.y })),
+      chests: game.chests,
     };
     const msg: ServerMessage = {
       type: ServerMessageType.StateUpdate,
@@ -79,7 +108,7 @@ function broadcastUpdates(roomId: RoomId) {
 
 setInterval(() => {
   states.forEach(({ game }, roomId) => {
-    game.players.forEach((player) => {
+    game.players.forEach(player => {
       if (player.direction === Direction.Up) {
         const nextTile = pixelToTile(player.x, player.y - PLAYER_SPEED);
         if (isBeachTile(nextTile)) {
--- shared/state.ts
+++ shared/state.ts
@@ -4,6 +4,21 @@ export type Player = {
   y: number;
 };
 
+export enum Difficulty {
+  low,
+  med,
+  hard,
+}
+
+export type Chest = {
+  id: string;
+  x: number;
+  y: number;
+  difficulty: Difficulty;
+  reward: number;
+};
+
 export type GameState = {
   players: Player[];
+  chests: Chest[];
 };
